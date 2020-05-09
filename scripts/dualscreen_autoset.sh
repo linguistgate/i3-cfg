4de3afdafbcc6cab273fe71b4760389a15df3a16
owl <m2eadams@gmail.com>
2020-05-08 23:21:13 -0600

    Initial commit

---
+++ scripts/dualscreen_autoset.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+#
+# Dual monitor support
+
+if xrandr | grep -q 'eDP-1 connected' && xrandr | grep -q 'VGA-1 connected'
+then
+    xrandr -s 0 --dpi 90
+    #xrandr --dpi 80 --output VGA1 --rotate left --mode 1280x1024 --right-of eDP1
+    xrandr --dpi 60 --output VGA-1 --rotate normal --mode 1920x1080 --left-of eDP-1
+    #xrandr --dpi 100 --output VGA1 --mode 1920x1080 --left-of eDP1
+fi
+
+# if xrandr | grep -q 'DVI-0 connected' && xrandr | grep -q 'DVI-1 connected'
+# then
+# 	xrandr --output DVI-0 --auto --left-of DVI-1
+
+# fi
+
+
+# if xrandr | grep -q 'DVI-I-1 connected' && xrandr | grep -q 'DVI-I-2 connected'
+# then
+# 	xrandr --output DVI-I-1 --auto --left-of DVI-I-2
+
+# fi
+
+
+
+# if xrandr | grep -q 'DVI-I-1 connected' && xrandr | grep -q 'VGA-1 connected'
+# then
+# 	xrandr --output DVI-I-1 --auto --left-of VGA-1
+
+# fi
+
+
+# if xrandr | grep -q 'DVI-I-0 connected' && xrandr | grep -q 'VGA-0 connected'
+# then
+# 	xrandr --output DVI-I-0 --auto --left-of VGA-0
+
+# fi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
